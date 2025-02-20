525b474e9b1611b01d486668fdc99aa5c1d28db8
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-02-20 23:13:01 +0530

    Main page background colour add

--- client/src/pages/home/HomePage/Home.jsx
+++ client/src/pages/home/HomePage/Home.jsx
@@ -1,28 +1,27 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router";
 import AuthContext from "../../../Context/AuthContext";
 
 const Home = () => {
   const { logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const onLogout = async () => {
-    await logOut();
-    navigate("/");
+  const handleLogout = () => {
+    logOut();
   };
   return (
-    <div>
-      <p>HomePage</p>
-      <div>
-        <button
-          onClick={() => {
-            onLogout();
-          }}
-          className="block w-full  px-4 py-2 hover:bg-teal-100"
-        >
-          Logout
-        </button>
+    <main className="min-h-screen bg-gradient-to-br from-teal-400 via-fuchsia-500 to-yellow-500 py-8 px-4">
+      <div className="w-10/12 mx-auto">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-4xl font-bold mb-8 text-center text-white">
+            Task Board
+          </h1>
+          <button
+            onClick={handleLogout}
+            className="bg-red-500 cursor-pointer text-white px-4 py-2 rounded-md mb-4"
+          >
+            LogOut
+          </button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
